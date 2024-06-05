3ff5b4d8324d785d6a911cbe3853c0dba901cd99
Hector Barazorda <ph.barazorda@gmail.com>
2024-06-05 04:24:04 -0500

    feat: create page.tsx, Alemana.tsx and ChartAlemana.tsx

---
+++ src/app/amortizacion-alemana/page.tsx
@@ -0,0 +1,18 @@
+import Header from "@/app/components/Header";
+import ChartAlemana from "../components/ChartAlemana";
+import GermanComponent from "../components/Alemana";
+
+export default function AmortizationGerman() {
+  return (
+    <div>
+      <Header />
+      <h2 className="my-10 text-center text-2xl font-bold">
+        Amortizacion Alemana
+      </h2>
+
+      <main className="flex min-h-screen  flex-wrap  justify-center gap-9 p-10">
+        <GermanComponent />
+      </main>
+    </div>
+  );
+}
---
+++ src/app/components/Alemana.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import ChartFrances from "./ChartFrances";
+import ChartAlemana from "./ChartAlemana";
+
+function PMT(
+  tasaInteresAnual: number,
+  numeroPeriodos: number,
+  capital: number
+) {
+  const tasaInteresMensual = tasaInteresAnual / 12 / 100;
+
+  if (numeroPeriodos <= 0 || tasaInteresMensual <= 0) {
+    return 0; // Maneja casos inválidos
+  }
+
+  const pagoMensual =
+    (capital * tasaInteresMensual) /
+    (1 - Math.pow(1 + tasaInteresMensual, -numeroPeriodos));
+  return pagoMensual;
+}
+function rateMonth(capital: number, rate: number, time: number) {
+  return (rate / 100 / time) * capital;
+}
+
+export default function GermanComponent() {
+  const [rate, setRate] = useState(11);
+  const [time, setTime] = useState(12);
+  const [capital, setCapital] = useState(300);
+  const [total, setTotal] = useState(capital);
+  const [pmt, setPmt] = useState(0);
+  const [rate2, setRate2] = useState(11);
+  useEffect(() => {
+    setTotal(((capital * rate) / 100) * time);
+
+    setPmt(PMT(rate, time, capital));
+    setRate2(rateMonth(capital, rate, time));
+  }, [rate, time, capital, rate2]);
+  return (
+    <main className="flex flex-col items-center justify-center max-h-screen bg-gray-100 dark:bg-gray-950 p-4">
+      <div className="max-w-sm md:max-w-md w-full bg-white dark:bg-gray-900 rounded-lg shadow-lg p-6 space-y-6">
+        <form className="space-y-4">
+          <div>
+            <label
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              htmlFor="capital"
+            >
+              Capital:
+            </label>
+            <input
+              value={capital}
+              onChange={(e) => setCapital(Number(e.target.valueAsNumber))}
+              className="mt-1 text-black block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              id="capital"
+              name="capital"
+              placeholder="Ingresa el capital"
+              type="number"
+            />
+          </div>
+          <div>
+            <label
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              htmlFor="rate"
+            >
+              Tasa de Interés:
+            </label>
+            <input
+              value={rate}
+              onChange={(e) => setRate(Number(e.target.valueAsNumber))}
+              className="mt-1 text-black block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              id="rate"
+              name="rate"
+              placeholder="Ingresa la tasa de interés"
+              type="number"
+            />
+          </div>
+          <div>
+            <label
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+              htmlFor="time"
+            >
+              Tiempo en meses:
+            </label>
+            <input
+              value={time}
+              onChange={(e) => setTime(Number(e.target.valueAsNumber))}
+              className="mt-1 text-black block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              id="time"
+              name="time"
+              placeholder="Ingresa el tiempo"
+              type="number"
+            />
+          </div>
+        </form>
+        <div className="bg-gray-100 dark:bg-gray-800 rounded-md p-4 space-y-2">
+          <p className="text-gray-700 dark:text-gray-300">
+            el pago mensual es:
+            <span className="font-bold">
+              S./
+              {Math.round(
+                ((capital * (rate / 100)) / time + Number.EPSILON) * 100
+              ) /
+                100 +
+                capital / time}
+            </span>
+          </p>
+          <p className="text-gray-700 dark:text-gray-300">
+            el primer interes mensual es:
+            <span className="font-bold">
+              S./
+              {Math.round(
+                ((capital * (rate / 100)) / time + Number.EPSILON) * 100
+              ) / 100}
+            </span>
+          </p>
+          <div className="w-full max-w-md mx-auto">
+            <ChartAlemana
+              pmt={pmt}
+              capital={capital}
+              rate={rate2}
+              time={time}
+            />
+          </div>
+          <h2 className="text-black">Amortizacion alemana:{capital / time}</h2>
+        </div>
+      </div>
+    </main>
+  );
+}
